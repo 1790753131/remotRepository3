7c345cc187915f3e7a9063c04a719d6f8b8344fc
1790753131 <1790753131@qq.com>
2023-05-27 22:03:22 +0800

    demo

--- xczx-demo/src/main/java/com/xczx/XczxApplication.java
+++ xczx-demo/src/main/java/com/xczx/XczxApplication.java
@@ -2,12 +2,13 @@ package com.xczx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class XczxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XczxApplication.class, args);
+		System.out.println("demo");
 	}
-
 }
